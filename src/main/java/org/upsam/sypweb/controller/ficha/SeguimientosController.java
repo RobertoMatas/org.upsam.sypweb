ba5371cd9945d544ffed6294e88d228f951e16c9
IIIRepublica <pepinoglande@gmail.com>
2012-05-08 17:49:44 +0200

    Nuevo controller

---
+++ src/main/java/org/upsam/sypweb/controller/ficha/SeguimientosController.java
@@ -0,0 +1,10 @@
+package org.upsam.sypweb.controller.ficha;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ficha/seg")
+public class SeguimientosController {
+
+}
